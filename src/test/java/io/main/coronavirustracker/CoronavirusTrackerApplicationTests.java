d1a75ba4e6f70d714d98131388b570ccbd7a3520
orange006 <alexeyefimov15@gmail.com>
2021-11-21 09:19:48 +0200

    Initial commit.

---
+++ src/test/java/io/main/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.main.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
